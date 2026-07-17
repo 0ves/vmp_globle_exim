ca90a673f6124297ac7d8dfa372cbe8a564e8dc3
oves nadaf <ovesnadaf@gmail.com>
2026-07-17 15:16:49 +0530

    import fix

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { ArrowRight } from "lucide-react";
+// import { ArrowRight } from "lucide-react";
 import { productCategories } from "../data/products";
 
 export default function Products() {
